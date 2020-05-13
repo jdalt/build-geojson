bd56bc9c8e32a83ff74952652ce70c23b72b0041
Jacob Dalton <jacob.dalton@sentera.com>
2020-05-13 18:10:42 -0500

    optionally open generated geojson in geojson.io

--- bin/cli.js
+++ bin/cli.js
@@ -8,12 +8,14 @@ const {
   buildPointFeatureCollection,
   generateRandomPointSquare
 } = require('./../src/points')
+const { exec } = require('child_process')
 
 program
   .version('0.0.1')
   .requiredOption('-b, --bbox <[s,w,n,e]>', 'bbox in which to limit points', JSON.parse)
   .option('-c, --count <number>', 'number of points to create')
   .option('-r, --random', 'place points randomly')
+  .option('-o, --open', 'output to geojson.io url')
   .parse(process.argv)
 
 const count = program.count || 1000
@@ -21,4 +23,10 @@ const coordList = program.random ?
   generateRandomPointSquare(program.bbox, count) : generatePointSquare(program.bbox, count)
 const geojson = buildPointFeatureCollection(coordList)
 
-console.log(JSON.stringify(geojson, null, 2))
+if (program.open) {
+  const geojsonStr = encodeURI(JSON.stringify(geojson))
+  const url = 'http://geojson.io/#data=data:application/json,' + geojsonStr
+  exec(`open ${url}`)
+} else {
+  console.log(JSON.stringify(geojson, null, 2))
+}
